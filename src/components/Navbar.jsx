42c5cfa7847f87b355a7d56bdea4fac26bc14599
joeabouserhal <joeabouserhal77777@gmail.com>
2022-08-19 06:38:37 +0300

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <h1>QR Code Generator</h1>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
